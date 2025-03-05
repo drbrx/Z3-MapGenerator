651bf19039bf8fc11f226cb6a8375666c5a7205c
drbrx <80510jb@gmail.com>
2025-03-05 10:38:09 +0100

    data loading

--- main.py
+++ main.py
@@ -1,22 +1,53 @@
-# region imports and setup
+# region imports
+
 import random
 from z3 import *
 import json
 
-
-with open(sys.argv[1], "r") as file:
+from cell import Cell
+from room import Room
+#endregion
+# region load config
+with open(sys.argv[2], "r") as file:
     data = json.load(file)
 
 cellTypes = {}
 for cellType in data["cells"]:
-    cellTypes[cellType["symbol"]] = dict(
-        name=cellType["name"], isTraversable=cellType["isTraversable"]
+    cellTypes[cellType["symbol"]] = Cell(
+        cellType["name"], cellType["isTraversable"], cellType["symbol"]
     )
-print(cellTypes)
+# print(cellTypes)
 
 roomTypes = {}
 for roomType in data["rooms"]:
-    roomTypes[roomType["name"]] = [
-        [cellTypes[symbol] for symbol in row] for row in roomType["map"]
-    ]
-print(roomTypes)
+    roomTypes[roomType["name"]] = Room(
+        roomType["name"],
+        [[cellTypes[symbol] for symbol in row] for row in roomType["map"]],
+    )
+# print(roomTypes)
+
+maxX = 0
+maxY = 0
+entryPos = None
+exitPos = None
+with open(sys.argv[1], "r") as file:
+    for line in file:
+        maxX = min(maxX or len(line), len(line))
+        x = 0
+        for char in line:
+            if char == ".":
+                pass
+            elif char == "i":
+                if entryPos:
+                    raise Exception("Entry point already set")
+                else:
+                    entryPos = dict(x=x, y=maxY)
+            elif char == "o":
+                if exitPos:
+                    raise Exception("Exit point already set")
+                else:
+                    exitPos = dict(x=x, y=maxY)
+            x += 1
+        maxY += 1
+# endregion
+
