ff01ce7ca0e749942a48bab21d865628f4ab6329
drbrx <80510jb@gmail.com>
2025-03-07 15:16:36 +0100

    basereachability constraints

--- main.py
+++ main.py
@@ -17,6 +17,9 @@ for cellType in data["cells"]:
     cellTypes[cellType["symbol"]] = Cell(
         cellType["name"], cellType["isTraversable"], cellType["symbol"]
     )
+
+ENTRANCE_SYMBOL = "i"
+EXIT_SYMBOL = "o"
 # print(cellTypes)
 
 gridHeight = 0
@@ -36,18 +39,75 @@ with open(sys.argv[1], "r") as file:
 solver = Solver()
 variables = {}
 constraints = []
-
+# init variables first to allow referencing
 for i in range(gridHeight):
     for j in range(gridLength):
-        variables[f"{i}x{j}"] = Int(f"{i}x{j}")
+        variables[f"{i}x{j}_Cell"] = Int(f"{i}x{j}_Cell")
+        variables[f"{i}x{j}_Trav"] = Bool(f"{i}x{j}_Trav")
+        variables[f"{i}x{j}_ReachDist"] = Int(f"{i}x{j}_ReachDist")
+        variables[f"{i}x{j}_MinAdjReachDist"] = Int(f"{i}x{j}_MinAdjReachDist")
+
+# define grid and base properties
+for i in range(gridHeight):
+    for j in range(gridLength):
         constraints.append(
-            And(variables[f"{i}x{j}"] >=0, variables[f"{i}x{j}"] < len(cellTypes.keys()))
+            variables[f"{i}x{j}_Cell"]
+            == list(cellTypes.keys()).index(grid[i][j].symbol)
+        )
+        constraints.append(variables[f"{i}x{j}_Trav"] == grid[i][j].isTraversable)
+
+        adjReachable = []
+        if i > 0:
+            adjReachable.append(variables[f"{i-1}x{j}_ReachDist"])
+        if i < gridHeight - 1:
+            adjReachable.append(variables[f"{i+1}x{j}_ReachDist"])
+        if j > 0:
+            adjReachable.append(variables[f"{i}x{j-1}_ReachDist"])
+        if j < gridLength - 1:
+            adjReachable.append(variables[f"{i}x{j+1}_ReachDist"])
+
+        minDist = adjReachable[0]
+        for n in adjReachable[1:]:
+            minDist = If(n < minDist, n, minDist)
+        constraints.append(variables[f"{i}x{j}_MinAdjReachDist"] == minDist)
+
+        constraints.append(
+            And(
+                variables[f"{i}x{j}_ReachDist"] >= -1,
+                Implies(
+                    variables[f"{i}x{j}_Cell"]
+                    == list(cellTypes.keys()).index(ENTRANCE_SYMBOL),
+                    variables[f"{i}x{j}_ReachDist"] == 0,
+                ),
+                Implies(
+                    variables[f"{i}x{j}_ReachDist"] == 0,
+                    variables[f"{i}x{j}_Cell"]
+                    == list(cellTypes.keys()).index(ENTRANCE_SYMBOL),
+                ),
+                Implies(
+                    Not(variables[f"{i}x{j}_Trav"]),
+                    variables[f"{i}x{j}_ReachDist"] == -1,
+                ),
+                variables[f"{i}x{j}_ReachDist"]
+                == If(
+                    variables[f"{i}x{j}_MinAdjReachDist"] == -1,
+                    -1,
+                    variables[f"{i}x{j}_MinAdjReachDist"] + 1,
+                ),
+            )
         )
 
+
 base_rules = And(constraints)
-base_rules = simplify(base_rules)
+# base_rules = simplify(base_rules)
 solver.add(base_rules)
 solver.push()
 
 print(solver.assertions())
+if solver.check() == sat:
+    model = solver.model()
+    print("Solution:\n")
+    print(model)
+else:
+    print("Unsatisfiable")
 # endregion
